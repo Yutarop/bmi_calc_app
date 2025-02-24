417f808d5faa297e208bfcf07777dc51e3e87e22
Yutaro <katoyutaro1122@icloud.com>
2025-02-24 21:58:55 +0900

    organize widgets in reusable_card and input_page files

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,15 +4,22 @@ import 'icon_content.dart';
 import 'reusable_card.dart';
 
 const bottomContainerHeight = 60.0;
-const reusableCardColor = 0xFF1D1E33;
-const bottomContainerColor = 0xFFEB1555;
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
+const bottomContainerColor = Color(0xFFEB1555);
+
+enum GenderType {male, female,}
 
 class InputPage extends StatefulWidget {
+
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+
+  GenderType? selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,36 +33,46 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: ReusableCard(
-                      colour: Color(reusableCardColor),
+                      colour: selectedGender == GenderType.male ? activeCardColor : inactiveCardColor,
                     cardChild: IconContent(iconText: 'MALE', iconShape: FontAwesomeIcons.mars, shouldApplyPadding: true,),
+                    onPress: (){
+                        setState(() {
+                          selectedGender = GenderType.male;
+                        });
+                    }
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    colour: Color(reusableCardColor),
+                    colour: selectedGender == GenderType.female ? activeCardColor : inactiveCardColor,
                     cardChild: IconContent(iconText: 'FEMALE', iconShape: FontAwesomeIcons.venus, shouldApplyPadding: false,),
+                    onPress: (){
+                      setState(() {
+                        selectedGender = GenderType.female;
+                      });
+                    },
                   ),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ReusableCard(colour: Color(reusableCardColor),),
+            child: ReusableCard(colour: inactiveCardColor,),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(colour: Color(reusableCardColor),),
+                  child: ReusableCard(colour: inactiveCardColor,),
                 ),
                 Expanded(
-                  child: ReusableCard(colour: Color(reusableCardColor),),
+                  child: ReusableCard(colour: inactiveCardColor,),
                 ),
               ],
             ),
           ),
           Container(
-            color: Color(bottomContainerColor),
+            color: bottomContainerColor,
             margin: EdgeInsets.only(top: 10),
             width: double.infinity,
             height: bottomContainerHeight,
